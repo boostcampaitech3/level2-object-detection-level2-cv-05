8626067b5752d41bc962673ce27fd058aacfd0dd
Kyubum Shin <kbum0617@naver.com>
2022-03-23 10:35:31 +0900

    [Feat] Upload configs
    upload cascade_rcnn_swin_Base_fpn.py
    upload cascade_rcnn_swin_Base_fpn_DIoU.py
    upload cascade_rcnn_swin_Base_fpn_CIoU.py
    fix swin_runtime.py
    rename scheduler_swin.py swin_scheduler.py

--- Swin_Transformer_Object_Detection/configs/p_stage/__base__/cascade_rcnn_swin_fpn.py
+++ Swin_Transformer_Object_Detection/configs/p_stage/__base__/cascade_rcnn_swin_fpn.py
@@ -40,6 +40,14 @@ model = dict(
             type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0),
         loss_bbox=dict(type='SmoothL1Loss', beta=1.0 / 9.0, loss_weight=1.0)),
     roi_head=dict(
+        type='CascadeRoIHead',
+        num_stages=3,
+        stage_loss_weights=[1, 0.5, 0.25],
+        bbox_roi_extractor=dict(
+            type='SingleRoIExtractor',
+            roi_layer=dict(type='RoIAlign', output_size=7, sampling_ratio=0),
+            out_channels=256,
+            featmap_strides=[4, 8, 16, 32]),
         bbox_head=[
             dict(
                 type='ConvFCBBoxHead',
